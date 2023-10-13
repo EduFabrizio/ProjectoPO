3b587873760e47885e93c7f8b675eb6fe331609c
Eduardo Fabrizio <eduardofabrizio@MacBook-Air-de-Eduardo.local>
2023-10-13 13:30:13 +0100

    DoNewCompleted

--- projeto/xxl/app/main/DoNew.java
+++ projeto/xxl/app/main/DoNew.java
@@ -3,6 +3,7 @@ package xxl.app.main;
 import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import xxl.app.exception.FileOpenFailedException;
 import xxl.core.Calculator;
 
 /**
@@ -12,10 +13,18 @@ class DoNew extends Command<Calculator> {
 
   DoNew(Calculator receiver) {
     super(Label.NEW, receiver);
+    addIntegerField("linhas", Message.lines());
+    addIntegerField("colunas", Message.columns());
   }
   
   @Override
   protected final void execute() throws CommandException {
-    // FIXME implement command
+    int linhas = integerField("linhas");
+    int colunas = integerField("colunas");
+    try {
+      _receiver.createNewSpreadsheet(linhas, colunas);
+    } catch (Exception e) {
+      throw new FileOpenFailedException(e);
+    } 
   }
 }
